5d68d0a5bcbc0c88cbd267dc132029be5f822e9b
alexanderkatz <katzkode@gmail.com>
2015-12-17 15:58:41 -0500

    SQL URL

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'connection': {
-  		host: 'sql.cs.oberlin.edu',
+  		host: 'mysql://b4eab3827be983:f2a8814e@us-cdbr-iron-east-03.cleardb.net/heroku_9ee959aeee4b050?reconnect=true',
   		dialect: 'mysql',
 
   		pool: {
@@ -15,3 +15,19 @@ module.exports = {
 	'password': 'collap'
 };
 
+// module.exports = {
+// 	'connection': {
+//   		host: 'localhost',
+//   		dialect: 'mysql',
+//
+//   		pool: {
+// 	    	max: 5,
+// 	    	min: 0,
+// 		    idle: 10000
+//   		},
+// 	},
+//
+// 	'user': 'root',
+// 	'dbname': 'collab',
+// 	'password': 'shimon'
+// };
